9af74f50b0706e7d324dd2fdf5bb3c5b5f879017
Your Name <you@example.com>
2018-04-23 12:48:26 -0700

    group daily queries

--- 1.js
+++
@@ -1,7 +0,0 @@
-var collName= 20180419
-// 2018-04-18 00:00:00 to 
-// 2018-04-18 23:59:59
-var dateRange= {"$gte":1524009600,"$lte":1524095999}
-// -----------------------------------------------------
-var queryDoc01={}; queryDoc01.signed_up_at = dateRange
-db.getCollection(`${collName}`).aggregate([ { "$match": queryDoc01 }, { "$group":{ "_id" : null,/* "Plan Names": { "$addToSet": { "api_plan_name" : "$custom_attributes.api_plan_name"}}, */ "SignUps" : {"$sum":1} } } ]) 
--- 4 5 6 .js
+++
@@ -1,8 +0,0 @@
-var collName= 20180420 
-// 2018-04-19 00:00:00 to 
-// 2018-04-19 23:59:59
-var dateRange= {"$gte":1524096000,"$lte":1524182399}
-// -----------------------------------------------------
-var queryDoc040506={}; queryDoc040506.signed_up_at = dateRange
-db.getCollection(`${collName}`)
-.aggregate([ { $match: { signed_up_at: dateRange } } ,{ $group: { _id : "$custom_attributes.api_experience_level", /* 'Plan Names': { $addToSet: { api_plan_name : "$custom_attributes.api_plan_name"}}, */ "Grouped by skill, sum" : {"$sum":1} } } ,{ $sort:{_id : 1} } ])
