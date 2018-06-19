1dc312f289f39a1b1927c972c6e0fc711ede817d
Jason Ashley <tradingbills@gmail.com>
2018-06-19 12:39:55 -0700

    ADDW-28.json object for API documentatoin by example, see

---
+++ APIDocsPaths_regex_objectToArray/regexFindSubset-Paths_getUniqueId.js
@@ -0,0 +1,27 @@
+db.getCollection('subset').aggregate([
+    {
+        $match: {
+            $and: [{
+                    "ENDPOINT:": {
+                        $regex: /Client\/ListUploadTerms/,
+                        '$options': 'i'
+                    }
+                }
+                    
+//                 ,{
+//                     "method.k": "patch"
+//                 }
+            ]
+        }
+    },
+    {
+        $addFields : { 
+            "tags":"",
+    "actualMethodUsed": "" ,
+    "actualURL": "",
+    "description" : "",
+    "docsURL":"",
+    "questons":[""]
+    }
+}
+])
