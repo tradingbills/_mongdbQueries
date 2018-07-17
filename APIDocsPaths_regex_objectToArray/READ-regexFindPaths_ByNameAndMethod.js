bf237c96cb7190060fc0d9e698818c656f9641b4
Jason Ashley <tradingbills@gmail.com>
2018-07-17 03:02:31 +0100

    search path by building queryDoc

--- APIDocsPaths_regex_objectToArray/READ-regexFindPaths_ByNameAndMethod.js
+++ APIDocsPaths_regex_objectToArray/READ-regexFindPaths_ByNameAndMethod.js
@@ -1,8 +1,7 @@
-var regex01 = /\/contact$/i
+var regex01 = /\/contact\/$/i
 
-var queryDoc01= {}
-queryDoc01["$match"]= {}
-queryDoc01["$match"]["path"] ={ $regex: regex01}
+var queryDoc01= {}
+queryDoc01["$match"]= {}
+queryDoc01["$match"]["path"] = { $regex: regex01}
 queryDoc01["$match"]["method"] = "get"
-
 db.getCollection('subset02').aggregate([ queryDoc01 ])
