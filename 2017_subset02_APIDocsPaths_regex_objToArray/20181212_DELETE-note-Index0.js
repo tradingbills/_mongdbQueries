30305e403e78a8436216682316a60ef969d2cdb6
Red Tablet <redTable@termux.com>
2018-12-12 21:43:09 -0800

    prettier on DELETEs

--- 2017_subset02_APIDocsPaths_regex_objToArray/20181212_DELETE-note-Index0.js
+++ 2017_subset02_APIDocsPaths_regex_objToArray/20181212_DELETE-note-Index0.js
@@ -1,8 +1,11 @@
-var objId = "5b2d17a95fc8779ec37a8d35"
-
-//     for multiple additions use $each https://docs.mongodb.com/manual/reference/operator/update/each/#up._S_each
-//     $addToSet :  	Adds elements to an array only if they do not already exist in the set.
-// var addDoc =  {"$addToSet":{"zenOrJira":zenOrJira}}
-//     $push : Adds an item to an array.
-// var addDoc =  {"$push":{"zenOrJira":zenOrJira}}
-db.getCollection('subset02').update({"_id": ObjectId(objId)}, {$pop:{"zenOrJira":-1}  })
+var objId = '';
+
+//     for multiple additions use $each https://docs.mongodb.com/manual/reference/operator/update/each/#up._S_each
+//     $addToSet :  	Adds elements to an array only if they do not already exist in the set.
+// var addDoc =  {"$addToSet":{"zenOrJira":zenOrJira}}
+//     $push : Adds an item to an array.
+// var addDoc =  {"$push":{"zenOrJira":zenOrJira}}
+db.getCollection('subset02').update(
+  {_id: ObjectId(objId)},
+  {$pop: {zenOrJira: -1}},
+);
--- 2017_subset02_APIDocsPaths_regex_objToArray/20181212_DELETE_notesByPOPOneOrManyObjectIds.js
+++ 2017_subset02_APIDocsPaths_regex_objToArray/20181212_DELETE_notesByPOPOneOrManyObjectIds.js
@@ -3,14 +3,17 @@
 // read above , csv, map, compilation = 
 */
 //var myStr="5b2d17a95fc8779ec37a8d39, 5b2d17a95fc8779ec37a8d40, 5b2d17a95fc8779ec37a8d34"
- var myStr = ""  //read above, string can have many
-var myArr=myStr.split(",") //trim in query string
-
-//     for multiple additions use $each https://docs.mongodb.com/manual/reference/operator/update/each/#up._S_each
-//     $addToSet :  	Adds elements to an array only if they do not already exist in the set.
-// var addDoc =  {"$addToSet":{"zenOrJira":zenOrJira}}
-//     $push : Adds an item to an array.
+var myStr = ''; //read above, string can have many
+var myArr = myStr.split(','); //trim in query string
+
+// for multiple additions use $each https://docs.mongodb.com/manual/reference/operator/update/each/#up._S_each
+//     $addToSet :  	Adds elements to an array only if they do not already exist in the set.
+// var addDoc =  {"$addToSet":{"zenOrJira":zenOrJira}}
+//     $push : Adds an item to an array.
 // var addDoc =  {"$push":{"zenOrJira":zenOrJira}}
-for( let objId of myArr){
-  db.getCollection('subset02').update({"_id": ObjectId(objId.trim())}, {$pop:{"zenOrJira":1}  })
-}
+for (let objId of myArr) {
+  db.getCollection('subset02').update(
+    {_id: ObjectId(objId.trim())},
+    {$pop: {zenOrJira: 1}},
+  );
+}
